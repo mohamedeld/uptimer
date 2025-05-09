7075185072cbd01a0476e9f3da8a4c154f57de24
mohamedeld <mohamed.azoz20010@gmail.com>
2025-05-09 16:25:56 +0300

    initial commit

---
+++ src/server/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+
+
+const sequelize = new Sequelize(process.env.POSTGRES_DB as string) // Example for postgres
+
+const connectToDB = async ()=>{
+    try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+}
+
+module.exports = connectToDB;
